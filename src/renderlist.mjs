48e93d81eff236d4e752ee42dca8db26269367dc
Florian Morel <ayamflow@gmail.com>
2021-01-22 22:30:41 +0100

    RenderList: fix multiple children

--- src/renderlist.mjs
+++ src/renderlist.mjs
@@ -3,12 +3,16 @@ export class RenderList {
         this.objects = []
     }
 
+    findChildren(parent) {
+        if (parent.render) this.objects.push(parent)
+        parent.children.forEach(child => {
+            if (child.children) this.findChildren(child)
+            else if (child.render) this.objects.push(child)
+        })
+    }
+
     setFrom(parent) {
         this.objects.length = 0
-        this.objects.push(parent)
-        parent.children.forEach(child => {
-            if (child.children) this.setFrom(child)
-            else this.objects.push(child)
-        })
+        this.findChildren(parent)
     }
 }
