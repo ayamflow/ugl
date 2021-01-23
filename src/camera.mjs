bed123218ad2fb6aefb8f068600e3a55ebaf9a27
Florian Morel <ayamflow@gmail.com>
2021-01-23 15:26:57 +0100

    Camera: fix .perspective and .orthographic methods

--- src/camera.mjs
+++ src/camera.mjs
@@ -14,9 +14,9 @@ export class Camera extends Transform {
     perspective({fov, aspect, near, far}) {
         // https://webgl2fundamentals.org/webgl/lessons/webgl-3d-perspective.html
         var vFOV = Math.tan(Math.PI * 0.5 - 0.5 * fov)
-        var rangeInv = 1.0 / (near - far)
+        var rangeInv = 1 / (near - far)
  
-        this.projectionMatrix.copy([
+        this.projectionMatrix.set([
             vFOV / aspect, 0, 0, 0,
             0, vFOV, 0, 0,
             0, 0, (near + far) * rangeInv, -1,
@@ -26,10 +26,10 @@ export class Camera extends Transform {
 
     orthographic({left, right, top, bottom, near, far}) {
         // https://webgl2fundamentals.org/webgl/lessons/webgl-3d-orthographic.html
-        this.projectionMatrix.copy([
+        this.projectionMatrix.set([
             2 / (right - left), 0, 0, 0,
             0, 2 / (top - bottom), 0, 0,
-            0, 0, 2 / (near - far), 0,
+            0, 0, -2 / (near - far), 0,
        
             (left + right) / (left - right),
             (bottom + top) / (bottom - top),
